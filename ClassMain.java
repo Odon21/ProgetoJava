17cf5c9cba37187e9808e6802fe5eb644e1bd696
Hack-21 <82946332+Odon21@users.noreply.github.com>
2024-06-10 14:46:15 +0200

    Update ClassMain.java

--- ClassMain.java
+++ ClassMain.java
@@ -7,23 +7,21 @@ public class ClassMain {
 		Itens item1 = new Itens();
 		Consultas cn1 = new Consultas();
 		cn1.marcarConsulta();
-		//item1.adicionarIten();
-		//item1.listarItensDisponiveis();
-		//item1.removerItem();
-		//Login L1 = new Login();
-		//Funcionario F1 = new Funcionario();
-		//L1.cadastrarFuncionario();
-		//L1.listarFuncionarios();
-		//L1.cadastrarPaciente();
-		//L1.listarPacientes();
-		//System.out.println("Credeciais estao corretos: "+L1.login());
-		//L1.cadastrarPaciente();
-		//L1.listarPacientes();
-		//L1.removerFuncionario();
-		//L1.removerPaciente();
-		//L1.localizarPaciente();
-		//I1.ideiasBemEstarDeSaude();
-		//information.comoPrevenirDoencas();
+		item1.adicionarIten();
+		item1.listarItensDisponiveis();
+		item1.removerItem();
+		L1.login();
+		L1.cadastrarFuncionario();
+		L1.listarFuncionarios();
+		L1.cadastrarPaciente();
+		L1.listarPacientes();
+		L1.cadastrarPaciente();
+		L1.listarPacientes();
+		L1.removerFuncionario();
+		L1.removerPaciente();
+		L1.localizarPaciente();
+		information.ideiasBemEstarDeSaude();
+		information.comoPrevenirDoencas();
 		
 	}
 }
